d2b48d294138f64aaa092372cf0a2e460d947a8b
Bjules <jules.besson@ynov.com>
2024-03-08 12:42:57 +0100

    d

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import '../globals.css'
+
+//n'affiche pas la navbar ou le footer
+export const metadata = {
+    title: "Threads",
+    description: "next js 13 application",
+};
+
+const inter = Inter({subsets : ["latin"]})
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
+}
